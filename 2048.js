d23d3d4bfca2465d8e5ffd7cb73e53ab6e5ce4a5
lucasKhane <dani.kn17.rjch@gmail.com>
2016-04-15 12:37:27 +0200

    Fillholes() hecha. Falta que si no se mueve nada no me de numero. La funcion de sumar. La puntuacion. Y el cambio de colores segun el numero

--- 2048.js
+++ 2048.js
@@ -28,6 +28,323 @@ getemptycell = function(){
   return cellname
 }
 
+fillholes = function(direction){
+  //Hasta las narices del algoritmo este---MEJORAR
+  for (var i=0; i<3; i++){
+    console.log(direction);
+      switch(direction) {
+        case "right":
+            if (($("#4").text() != "")||($("#3").text() != "")||($("#2").text() != "")||($("#1").text() != "")){
+              if ($("#4").text() == ""){
+                $("#4").text($("#3").text());
+                $("#3").text($("#2").text());
+                $("#2").text($("#1").text());
+                $("#1").text("");
+              }
+              if ($("#3").text() == ""){
+                $("#3").text($("#2").text());
+                $("#2").text($("#1").text());
+                $("#1").text("");
+              }
+              if ($("#2").text() == ""){
+                $("#2").text($("#1").text());
+                $("#1").text("");
+              }
+              //Y si la de la izquierda del todo esta vacia no pasa nada
+            }
+            if (($("#8").text() != "")||($("#7").text() != "")||($("#6").text() != "")||($("#5").text() != "")){
+              if ($("#8").text() == ""){
+                $("#8").text($("#7").text());
+                $("#7").text($("#6").text());
+                $("#6").text($("#5").text());
+                $("#5").text("");
+              }
+              if ($("#7").text() == ""){
+                $("#7").text($("#6").text());
+                $("#6").text($("#5").text());
+                $("#5").text("");
+              }
+              if ($("#6").text() == ""){
+                $("#6").text($("#5").text());
+                $("#5").text("");
+              }
+              //Y si la de la izquierda del todo esta vacia no pasa nada
+            }
+            if (($("#12").text() != "")||($("#11").text() != "")||($("#10").text() != "")||($("#9").text() != "")){
+              if ($("#12").text() == ""){
+                $("#12").text($("#11").text());
+                $("#11").text($("#10").text());
+                $("#10").text($("#9").text());
+                $("#9").text("");
+              }
+              if ($("#11").text() == ""){
+                $("#11").text($("#10").text());
+                $("#10").text($("#9").text());
+                $("#9").text("");
+              }
+              if ($("#10").text() == ""){
+                $("#10").text($("#9").text());
+                $("#9").text("");
+              }
+              //Y si la de la izquierda del todo esta vacia no pasa nada
+            }
+            if (($("#16").text() != "")||($("#15").text() != "")||($("#14").text() != "")||($("#13").text() != "")){
+              if ($("#16").text() == ""){
+                $("#16").text($("#15").text());
+                $("#15").text($("#14").text());
+                $("#14").text($("#13").text());
+                $("#13").text("");
+              }
+              if ($("#15").text() == ""){
+                $("#15").text($("#14").text());
+                $("#14").text($("#13").text());
+                $("#13").text("");
+              }
+              if ($("#14").text() == ""){
+                $("#14").text($("#13").text());
+                $("#13").text("");
+              }
+              //Y si la de la izquierda del todo esta vacia no pasa nada
+            }
+            break;
+        case "left":
+            if (($("#1").text() != "")||($("#2").text() != "")||($("#3").text() != "")||($("#4").text() != "")){
+              if ($("#1").text() == ""){
+                $("#1").text($("#2").text());
+                $("#2").text($("#3").text());
+                $("#3").text($("#4").text());
+                $("#4").text("");
+              }
+              if ($("#2").text() == ""){
+                $("#2").text($("#3").text());
+                $("#3").text($("#4").text());
+                $("#4").text("");
+              }
+              if ($("#3").text() == ""){
+                $("#3").text($("#4").text());
+                $("#4").text("");
+              }
+              //Y si la derecha del todo esta vacía no pasa nada
+            }
+            if (($("#5").text() != "")||($("#6").text() != "")||($("#7").text() != "")||($("#4").text() != "")){
+              if ($("#5").text() == ""){
+                $("#5").text($("#6").text());
+                $("#6").text($("#7").text());
+                $("#7").text($("#8").text());
+                $("#8").text("");
+              }
+              if ($("#6").text() == ""){
+                $("#6").text($("#7").text());
+                $("#7").text($("#8").text());
+                $("#8").text("");
+              }
+              if ($("#7").text() == ""){
+                $("#7").text($("#8").text());
+                $("#8").text("");
+              }
+              //Y si la derecha del todo esta vacía no pasa nada
+            }
+            if (($("#9").text() != "")||($("#10").text() != "")||($("#11").text() != "")||($("#12").text() != "")){
+              if ($("#9").text() == ""){
+                $("#9").text($("#10").text());
+                $("#10").text($("#11").text());
+                $("#11").text($("#12").text());
+                $("#12").text("");
+              }
+              if ($("#10").text() == ""){
+                $("#10").text($("#11").text());
+                $("#11").text($("#12").text());
+                $("#12").text("");
+              }
+              if ($("#11").text() == ""){
+                $("#11").text($("#12").text());
+                $("#12").text("");
+              }
+              //Y si la derecha del todo esta vacía no pasa nada
+            }
+            if (($("#13").text() != "")||($("#14").text() != "")||($("#15").text() != "")||($("#16").text() != "")){
+              if ($("#13").text() == ""){
+                $("#13").text($("#14").text());
+                $("#14").text($("#15").text());
+                $("#15").text($("#16").text());
+                $("#16").text("");
+              }
+              if ($("#14").text() == ""){
+                $("#14").text($("#15").text());
+                $("#15").text($("#16").text());
+                $("#16").text("");
+              }
+              if ($("#15").text() == ""){
+                $("#15").text($("#16").text());
+                $("#16").text("");
+              }
+              //Y si la derecha del todo esta vacía no pasa nada
+            }
+            break;
+        case "top":
+            if (($("#4").text() != "")||($("#8").text() != "")||($("#12").text() != "")||($("#16").text() != "")){
+              if ($("#4").text() == ""){
+                $("#4").text($("#8").text());
+                $("#8").text($("#12").text());
+                $("#12").text($("#16").text());
+                $("#16").text("");
+              }
+              if ($("#8").text() == ""){
+                $("#8").text($("#12").text());
+                $("#12").text($("#16").text());
+                $("#16").text("");
+              }
+              if ($("#12").text() == ""){
+                $("#12").text($("#16").text());
+                $("#16").text("");
+              }
+              //Y si el de abajo del todo esta vacio no pasa nada
+            }
+            if (($("#3").text() != "")||($("#7").text() != "")||($("#11").text() != "")||($("#15").text() != "")){
+              if ($("#3").text() == ""){
+                $("#3").text($("#7").text());
+                $("#7").text($("#11").text());
+                $("#11").text($("#15").text());
+                $("#15").text("");
+              }
+              if ($("#7").text() == ""){
+                $("#7").text($("#11").text());
+                $("#11").text($("#15").text());
+                $("#15").text("");
+              }
+              if ($("#11").text() == ""){
+                $("#11").text($("#15").text());
+                $("#15").text("");
+              }
+              //Y si el de abajo del todo esta vacio no pasa nada
+            }
+            if (($("#2").text() != "")||($("#6").text() != "")||($("#10").text() != "")||($("#14").text() != "")){
+              if ($("#2").text() == ""){
+                $("#2").text($("#6").text());
+                $("#6").text($("#10").text());
+                $("#10").text($("#14").text());
+                $("#14").text("");
+              }
+              if ($("#6").text() == ""){
+                $("#6").text($("#10").text());
+                $("#10").text($("#14").text());
+                $("#14").text("");
+              }
+              if ($("#10").text() == ""){
+                $("#10").text($("#14").text());
+                $("#14").text("");
+              }
+              //Y si el de abajo del todo esta vacio no pasa nada
+            }
+            if (($("#1").text() != "")||($("#5").text() != "")||($("#9").text() != "")||($("#13").text() != "")){
+              if ($("#1").text() == ""){
+                $("#1").text($("#5").text());
+                $("#5").text($("#9").text());
+                $("#9").text($("#13").text());
+                $("#13").text("");
+              }
+              if ($("#5").text() == ""){
+                $("#5").text($("#9").text());
+                $("#9").text($("#13").text());
+                $("#13").text("");
+              }
+              if ($("#9").text() == ""){
+                $("#9").text($("#13").text());
+                $("#13").text("");
+              }
+              //Y si el de abajo del todo esta vacio no pasa nada
+            }
+            break;
+        case "bot":
+            if (($("#16").text() != "")||($("#12").text() != "")||($("#8").text() != "")||($("#4").text() != "")){
+              if ($("#16").text() == ""){
+                $("#16").text($("#12").text());
+                $("#12").text($("#8").text());
+                $("#8").text($("#4").text());
+                $("#4").text("");
+              }
+              if ($("#12").text() == ""){
+                $("#12").text($("#8").text());
+                $("#8").text($("#4").text());
+                $("#4").text("");
+              }
+              if ($("#8").text() == ""){
+                $("#8").text($("#4").text());
+                $("#4").text("");
+              }
+              //Y si el de arriba del todo esta vacio no pasa nada
+            }
+            break;
+            if (($("#15").text() != "")||($("#11").text() != "")||($("#7").text() != "")||($("#3").text() != "")){
+              if ($("#15").text() == ""){
+                $("#15").text($("#11").text());
+                $("#11").text($("#7").text());
+                $("#7").text($("#3").text());
+                $("#3").text("");
+              }
+              if ($("#11").text() == ""){
+                $("#11").text($("#7").text());
+                $("#7").text($("#3").text());
+                $("#3").text("");
+              }
+              if ($("#7").text() == ""){
+                $("#7").text($("#3").text());
+                $("#3").text("");
+              }
+              //Y si el de arriba del todo esta vacio no pasa nada
+            }
+            break;
+            if (($("#14").text() != "")||($("#10").text() != "")||($("#6").text() != "")||($("#2").text() != "")){
+              if ($("#14").text() == ""){
+                $("#14").text($("#10").text());
+                $("#10").text($("#6").text());
+                $("#6").text($("#2").text());
+                $("#2").text("");
+              }
+              if ($("#10").text() == ""){
+                $("#10").text($("#6").text());
+                $("#6").text($("#2").text());
+                $("#2").text("");
+              }
+              if ($("#6").text() == ""){
+                $("#6").text($("#2").text());
+                $("#2").text("");
+              }
+              //Y si el de arriba del todo esta vacio no pasa nada
+            }
+            break;
+            if (($("#13").text() != "")||($("#9").text() != "")||($("#5").text() != "")||($("#1").text() != "")){
+              if ($("#13").text() == ""){
+                $("#13").text($("#9").text());
+                $("#9").text($("#5").text());
+                $("#5").text($("#1").text());
+                $("#1").text("");
+              }
+              if ($("#9").text() == ""){
+                $("#9").text($("#5").text());
+                $("#5").text($("#1").text());
+                $("#1").text("");
+              }
+              if ($("#5").text() == ""){
+                $("#5").text($("#1").text());
+                $("#1").text("");
+              }
+              //Y si el de arriba del todo esta vacio no pasa nada
+            }
+            break;
+        default:
+          alert("PROBLEMA");
+      }
+  }
+}
+
+gotRigth = function(){
+  //$("#7").effect( "blind", {direction: "right"}, 1000 );
+  //if ($("#4").text != ""){
+  var prueba = "4";
+  console.log(prueba+"2");
+}
+
 $(document).ready(function(){
   var cellname = getemptycell();
   $(cellname).text(newnumber());
@@ -38,27 +355,30 @@ $(document).ready(function(){
     switch(e.which) {
         case 37: // left
           console.log("Izquierda");
+          fillholes("left");
           cellname = getemptycell();
           $(cellname).text(newnumber());
           break;
         case 38: // up
           console.log("Arriba");
+          fillholes("top");
           cellname = getemptycell();
           $(cellname).text(newnumber());
           break;
         case 39: // right
           console.log("Derecha");
+          fillholes("right");
           cellname = getemptycell();
           $(cellname).text(newnumber());
           break;
         case 40: // down
           console.log("Abajo");
+          fillholes("bot");
           cellname = getemptycell();
           $(cellname).text(newnumber());
           break;
         default: return; // exit this handler for other keys
     }
     e.preventDefault(); // prevent the default action (scroll / move caret)
-});
-
+  });
 });
