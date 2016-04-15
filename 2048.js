6b77b28065d5dad0869edc707cfe9993e2d05ad3
lucasKhane <dani.kn17.rjch@gmail.com>
2016-04-15 14:10:08 +0200

    sumnum Done. Rest fix one ) before +. I am gona play rugby now..after continue :-)

--- 2048.js
+++ 2048.js
@@ -338,11 +338,262 @@ fillholes = function(direction){
   }
 }
 
-gotRigth = function(){
-  //$("#7").effect( "blind", {direction: "right"}, 1000 );
-  //if ($("#4").text != ""){
-  var prueba = "4";
-  console.log(prueba+"2");
+
+
+sumNum= function(direction){
+    //Casos:
+    //Primero igual segundo -> y/o tercero igual cuarto
+    //Segundo igual tercero
+    //Tercero igual cuarto
+    switch (direction) {
+      case "right":
+        if ($("#4").text() == $("#3").text()){
+          $("#4").text(parseInt($("#4").text()+parseInt($("#3").text()));
+          $("#3").text("");
+          if ($("#2").text() == $("#1").text()) {
+            $("#2").text() = parseInt($("#2").text()+parseInt($("#1").text()));
+            $("#1").text("");
+          }
+        }else if ($("#3").text() == $("#2").text()) {
+          $("#3").text(parseInt($("#3").text())+parseInt($("#2").text()));
+          $("#2").text("");
+        }else if ($("#2").text() == $("#1").text()) {
+          $("#2").text(parseInt($("#2").text())+parseInt($("#1").text()));
+          $("#1").text("");
+        }
+
+        if ($("#8").text() == $("#7").text()){
+          $("#8").text(parseInt($("#8").text())+parseInt($("#7").text()));
+          $("#7").text("");
+          if ($("#6").text() == $("#5").text()) {
+            $("#6").text(parseInt($("#6").text())+parseInt($("#5").text()));
+            $("#5").text("");
+          }
+        }else if ($("#7").text() == $("#6").text()) {
+          $("#7").text(parseInt($("#7").text())+parseInt($("#6").text()));
+          $("#6").text("");
+        }else if ($("#6").text() == $("#5").text()) {
+          $("#6").text(parseInt($("#5").text())+parseInt($("#5").text()));
+          $("#5").text("");
+        }
+
+        if ($("#12").text() == $("11").text()){
+          $("#12").text(parseInt($("#12").text())+parseInt($("#11").text()));
+          $("#11").text("");
+          if ($("#10").text() == $("#9").text()) {
+            $("#10").text(parseInt($("#10").text())+parseInt($("#9").text()));
+            $("#9").text("");
+          }
+        }else if ($("#11").text() == $("#10").text()) {
+          $("#11").text(parseInt($("#11").text())+parseInt($("#10").text()));
+          $("#10").text("");
+        }else if ($("#10").text() == $("#9").text()) {
+          $("#10").text(parseInt($("#10").text())+parseInt($("#9").text()));
+          $("#9").text("");
+        }
+
+        if ($("#16").text() == $("#15").text()){
+          $("#16").text(parseInt($("#16").text())+parseInt($("#15").text()));
+          $("#15").text("");
+          if ($("#14").text() == $("#13").text()) {
+            $("#14").text(parseInt($("#14").text())+parseInt($("#13").text()));
+            $("#13").text("");
+          }
+        }else if ($("#15").text() == $("#14").text()) {
+          $("#15").text(parseInt($("#15").text())+parseInt($("#14").text()));
+          $("#14").text("");
+        }else if ($("#14").text() == $("#13").text()) {
+          $("#14").text(parseInt($("#14").text())+parseInt($("#13").text()));
+          $("#13").text("");
+        }
+        break;
+
+      case "left":
+          if ($("#1").text() == $("#2").text()){
+            $("#1").text(parseInt($("#1").text())+parseInt($("#2").text()));
+            $("#2").text("");
+            if ($("#3").text() == $("#4").text()) {
+              $("#3").text(parseInt($("#3").text())+parseInt($("#4").text()));
+              $("#4").text("");
+            }
+          }else if ($("#2").text() == $("#3").text()) {
+            $("#2").text(parseInt($("#2").text())+parseInt($("#3").text()));
+            $("#3").text("");
+          }else if ($("#3").text() == $("#4").text()) {
+            $("#3").text(parseInt($("#3").text())+parseInt($("#4").text()));
+            $("#4").text("");
+          }
+
+          if ($("#5").text() == $("#5").text()){
+            $("#5").text(parseInt($("#5").text())+parseInt($("#6").text()));
+            $("#6").text("");
+          if ($("#7").text() == $("#8").text()) {
+              $("#7").text(parseInt($("#7").text())+parseInt($("#8").text()));
+              $("#8").text("");
+            }
+          }else if ($("#6").text() == $("#7").text()) {
+            $("#6").text(parseInt($("#6").text())+parseInt($("#7").text()));
+            $("#7").text("");
+          }else if ($("#7").text() == $("#8").text()) {
+            $("#7").text(parseInt($("#7").text())+parseInt($("#8").text()));
+            $("#8").text("");
+          }
+
+          if ($("#9").text() == $("10").text()){
+            $("#9").text(parseInt($("#9").text())+parseInt($("#10").text()));
+            $("#10").text("");
+            if ($("#11").text() == $("#12").text()) {
+              $("#11").text(parseInt($("#11").text())+parseInt($("#12").text()));
+              $("#12").text("");
+            }
+          }else if ($("#10").text() == $("#11").text()) {
+            $("#10").text(parseInt($("#10").text())+parseInt($("#11").text()));
+            $("#11").text("");
+          }else if ($("#11").text() == $("#12").text()) {
+            $("#11").text(parseInt($("#11").text())+parseInt($("#12").text()));
+            $("#12").text("");
+          }
+
+          if ($("#13").text() == $("#14").text()){
+            $("#13").text(parseInt($("#13").text())+parseInt($("#14").text()));
+            $("#14").text("");
+            if ($("#15").text() == $("#16").text()) {
+              $("#15").text(parseInt($("#15").text())+parseInt($("#16").text()));
+              $("#16").text("");
+            }
+          }else if ($("#14").text() == $("#15").text()) {
+            $("#14").text(parseInt($("#14").text())+parseInt($("#15").text()));
+            $("#15").text("");
+          }else if ($("#15").text() == $("#16").text()) {
+            $("#15").text(parseInt($("#15").text())+parseInt($("#16").text()));
+            $("#16").text("");
+          }
+          break;
+      case "top":
+          if ($("#4").text() == $("#8").text()){
+            $("#4").text(parseInt($("#4").text())+parseInt($("#8").text()));
+            $("#8").text("");
+            if ($("#12").text() == $("#16").text()) {
+              $("#12").text(parseInt($("#12").text())+parseInt($("#16").text()));
+              $("#16").text("");
+            }
+          }else if ($("#8").text() == $("#12").text()) {
+            $("#8").text(parseInt($("#8").text())+parseInt($("#12").text()));
+            $("#12").text("");
+          }else if ($("#12").text() == $("#16").text()) {
+            $("#12").text(parseInt($("#12").text())+parseInt($("#16").text()));
+            $("#16").text("");
+          }
+
+          if ($("#3").text() == $("#7").text()){
+            $("#3").text(parseInt($("#3").text())+parseInt($("#7").text()));
+            $("#7").text("");
+            if ($("#11").text() == $("#15").text()) {
+              $("#11").text(parseInt($("#11").text())+parseInt($("#15").text()));
+              $("#15").text("");
+            }
+          }else if ($("#7").text() == $("#11").text()) {
+            $("#7").text(parseInt($("#7").text())+parseInt($("#11").text()));
+            $("#11").text("");
+          }else if ($("#11").text() == $("#15").text()) {
+            $("#11").text(parseInt($("#11").text())+parseInt($("#15").text()));
+            $("#15").text("");
+          }
+
+          if ($("#2").text() == $("6").text()){
+            $("#2").text(parseInt($("#2").text())+parseInt($("#6").text()));
+            $("#6").text("");
+            if ($("#10").text() == $("#14").text()) {
+              $("#10").text(parseInt($("#10").text())+parseInt($("#14").text()));
+              $("#14").text("");
+            }
+          }else if ($("#6").text() == $("#10").text()) {
+            $("#6").text(parseInt($("#6").text())+parseInt($("#10").text()));
+            $("#10").text("");
+          }else if ($("#10").text() == $("#14").text()) {
+            $("#10").text(parseInt($("#10").text())+parseInt($("#14").text()));
+            $("#14").text("");
+          }
+
+          if ($("#1").text() == $("#5").text()){
+            $("#1").text(parseInt($("#1").text())+parseInt($("#5").text()));
+            $("#5").text("");
+            if ($("#9").text() == $("#13").text()) {
+              $("#9").text(parseInt($("#9").text())+parseInt($("#13").text()));
+              $("#12").text("");
+            }
+          }else if ($("#5").text() == $("#9").text()) {
+            $("#5").text(parseInt($("#5").text())+parseInt($("#9").text()));
+            $("#9").text("");
+          }else if ($("#9").text() == $("#13").text()) {
+            $("#9").text(parseInt($("#9").text())+parseInt($("#13").text()));
+            $("#13").text("");
+          }
+          break;
+      case "bot":
+          if ($("#16").text() == $("#12").text()){
+            $("#16").text(parseInt($("#16").text())+parseInt($("#12").text()));
+            $("#12").text("");
+            if ($("#8").text() == $("#4").text()) {
+              $("#8").text(parseInt($("#8").text())+parseInt($("#4").text()));
+              $("#4").text("");
+            }
+          }else if ($("#12").text() == $("#8").text()) {
+            $("#12").text(parseInt($("#12").text())+parseInt($("#8").text()));
+            $("#8").text("");
+          }else if ($("#8").text() == $("#4").text()) {
+            $("#8").text(parseInt($("#8").text())+parseInt($("#4").text()));
+            $("#4").text("");
+          }
+
+          if ($("#15").text() == $("#11").text()){
+            $("#15").text(parseInt($("#15").text())+parseInt($("#11").text()));
+            $("#11").text("");
+          if ($("#7").text() == $("#3").text()) {
+              $("#7").text(parseInt($("#7").text())+parseInt($("#3").text()));
+              $("#3").text("");
+            }
+          }else if ($("#11").text() == $("#7").text()) {
+            $("#11").text(parseInt($("#11").text())+parseInt($("#7").text()));
+            $("#7").text("");
+          }else if ($("#7").text() == $("#3").text()) {
+            $("#7").text(parseInt($("#7").text())+parseInt($("#3").text()));
+            $("#3").text("");
+          }
+
+          if ($("#14").text() == $("10").text()){
+            $("#14").text(parseInt($("#14").text())+parseInt($("#10").text()));
+            $("#10").text("");
+            if ($("#6").text() == $("#2").text()) {
+              $("#6").text(parseInt($("#6").text())+parseInt($("#2").text()));
+              $("#2").text("");
+            }
+          }else if ($("#10").text() == $("#6").text()) {
+            $("#10").text(parseInt($("#10").text())+parseInt($("#6").text()));
+            $("#6").text("");
+          }else if ($("#6").text() == $("#2").text()) {
+            $("#6").text(parseInt($("#6").text())+parseInt($("#2").text()));
+            $("#2").text("");
+          }
+
+          if ($("#13").text() == $("#9").text()){
+            $("#13").text(parseInt($("#13").text())+parseInt($("#9").text()));
+            $("#9").text("");
+            if ($("#5").text() == $("#1").text()) {
+              $("#5").text(parseInt($("#5").text())+parseInt($("#1").text()));
+              $("#1").text("");
+            }
+          }else if ($("#9").text() == $("#5").text()) {
+            $("#9").text(parseInt($("#9").text())+parseInt($("#5").text()));
+            $("#5").text("");
+          }else if ($("#5").text() == $("#1").text()) {
+            $("#5").text(parseInt($("#5").text())+parseInt($("#1").text()));
+            $("#1").text("");
+          }
+          break;
+      default:
+          alert("PROBLEMA");
+    }
 }
 
 $(document).ready(function(){
